d2418ceb769dcaa92f6d801ad5605464574d58cc
20201tiimi0330 <miguelabreumatos@gmail.com>
2022-09-23 14:12:01 -0300

    PASSO 8

--- app/src/main/java/abreu/miguel/galeriapblica/MainActivity.java
+++ app/src/main/java/abreu/miguel/galeriapblica/MainActivity.java
@@ -2,11 +2,16 @@ package abreu.miguel.galeriapblica;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -29,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.btNav);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 vm.setNavigationOpSelected(item.getItemId());
@@ -44,59 +50,89 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return true;
             }
-
-            @Override
-            public void onResume() {
-                super.onResume();
-                List<String> permissions = new ArrayList<>();
-                permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
-                checkForPermissions(permissions);
-            }
-
-
-
-            private void checkForPermissions(List<String> permissions) {
-                List<String> permissionsNotGranted = new ArrayList<>();
-
-                if(permissionsNotGranted.size() > 0) {
-                }
-
-                else {
-                    MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
-                    int navigationOpSelected = vm.getNavigationOpSelected();
-                    bottomNavigationView.setSelectedItemId(navigationOpSelected);
-                }
-            }
-
-            @Override
-            public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-                super.onRequestPermissionsResult(requestCode, permissions,
-
-
-                if(permissionsRejected.size() > 0) {
-
-
-                }
-                else {
-                    MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
-                    int navigationOpSelected = vm.getNavigationOpSelected();
-                    bottomNavigationView.setSelectedItemId(navigationOpSelected);
-                }
-            }
-
-
-
-
-            private void setFragment(GridViewFragment gridViewFragment) {
-            }
-
-            private void setFragment(ListViewFragment listViewFragment) {
-            }
-
         });
     }
 
-         void setFragment(Fragment Fragment) {
+    @Override
+    public void onResume() {
+        super.onResume();
+        List<String> permissions = new ArrayList<>();
+        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        checkForPermissions(permissions);
+    }
+
+
+    private void checkForPermissions(List<String> permissions) {
+        List<String> permissionsNotGranted = new ArrayList<>();
+
+        for (String permission : permissions) {
+            if (!hasPermission(permission)) {
+                permissionsNotGranted.add(permission);
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (permissionsNotGranted.size() > 0) {
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
+            } else {
+                MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+                int navigationOpSelected = vm.getNavigationOpSelected();
+                bottomNavigationView.setSelectedItemId(navigationOpSelected);
+            }
+        }
+    }
+
+    private boolean hasPermission(String permission) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
+        }
+        return false;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        final List<String> permissionsRejected = new ArrayList<>();
+        if (requestCode == RESULT_REQUEST_PERMISSION) {
+
+            for (String permission : permissions) {
+                if (!hasPermission(permission)) {
+                    permissionsRejected.add(permission);
+                }
+            }
+        }
+
+        if (permissionsRejected.size() > 0) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+                    new AlertDialog.Builder(MainActivity.this).
+                            setMessage("Para usar essa app é preciso conceder essas permissões").
+                            setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestPermissions(permissionsRejected.toArray(new String[permissionsRejected.size()]), RESULT_REQUEST_PERMISSION);
+                                }
+                            }).create().show();
+                }
+            }
+        } else {
+            MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
+            int navigationOpSelected = vm.getNavigationOpSelected();
+            bottomNavigationView.setSelectedItemId(navigationOpSelected);
+        }
+
+    }
+
+
+    private void setFragment(GridViewFragment gridViewFragment) {
+    }
+
+    private void setFragment(ListViewFragment listViewFragment) {
+    }
+
+
+    void setFragment(Fragment Fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragContainer, fragment);
         fragmentTransaction.addToBackStack(null);
