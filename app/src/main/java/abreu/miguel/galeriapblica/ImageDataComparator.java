f409cc50fbd5a9b76b93b7cc702bfec77ee45533
20201tiimi0330 <miguelabreumatos@gmail.com>
2022-09-30 14:43:18 -0300

    finish

---
+++ app/src/main/java/abreu/miguel/galeriapblica/ImageDataComparator.java
@@ -0,0 +1,20 @@
+package abreu.miguel.galeriapblica;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+class ImageDataComparator extends DiffUtil.ItemCallback<ImageData> {
+    @Override
+    public boolean areItemsTheSame(@NonNull ImageData oldItem,
+                                   @NonNull ImageData newItem) {
+        // Id is unique.
+        return oldItem.uri.equals(newItem.uri);
+    }
+
+    @Override
+    public boolean areContentsTheSame(@NonNull ImageData oldItem,
+                                      @NonNull ImageData newItem) {
+        return oldItem.uri.equals(newItem.uri);
+    }
+}
+
--- app/src/main/java/abreu/miguel/galeriapblica/MainActivity.java
+++ app/src/main/java/abreu/miguel/galeriapblica/MainActivity.java
@@ -2,12 +2,14 @@ package abreu.miguel.galeriapblica;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.view.menu.MenuWrapperICS;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
@@ -24,6 +26,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
+    static int RESULT_REQUEST_PERMISSION = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +37,6 @@ public class MainActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.btNav);
         bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
-
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 vm.setNavigationOpSelected(item.getItemId());
@@ -50,31 +52,31 @@ public class MainActivity extends AppCompatActivity {
                 }
                 return true;
             }
+
         });
     }
 
     @Override
-    public void onResume() {
+    protected void onResume() {
         super.onResume();
         List<String> permissions = new ArrayList<>();
         permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE);
         checkForPermissions(permissions);
     }
 
-
     private void checkForPermissions(List<String> permissions) {
         List<String> permissionsNotGranted = new ArrayList<>();
 
-        for (String permission : permissions) {
-            if (!hasPermission(permission)) {
+        for(String permission : permissions) {
+            if( !hasPermission(permission)) {
                 permissionsNotGranted.add(permission);
             }
         }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (permissionsNotGranted.size() > 0) {
-                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]), RESULT_REQUEST_PERMISSION);
-            } else {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(permissionsNotGranted.size() > 0) {
+                requestPermissions(permissionsNotGranted.toArray(new String[permissionsNotGranted.size()]),RESULT_REQUEST_PERMISSION);
+            }else {
                 MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
                 int navigationOpSelected = vm.getNavigationOpSelected();
                 bottomNavigationView.setSelectedItemId(navigationOpSelected);
@@ -83,29 +85,30 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private boolean hasPermission(String permission) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             return ActivityCompat.checkSelfPermission(MainActivity.this, permission) == PackageManager.PERMISSION_GRANTED;
         }
         return false;
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
         final List<String> permissionsRejected = new ArrayList<>();
-        if (requestCode == RESULT_REQUEST_PERMISSION) {
+        if(requestCode == RESULT_REQUEST_PERMISSION) {
 
-            for (String permission : permissions) {
-                if (!hasPermission(permission)) {
+            for(String permission : permissions) {
+                if(!hasPermission(permission)) {
                     permissionsRejected.add(permission);
                 }
             }
         }
 
-        if (permissionsRejected.size() > 0) {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
+        if(permissionsRejected.size() > 0) {
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if(shouldShowRequestPermissionRationale(permissionsRejected.get(0))) {
                     new AlertDialog.Builder(MainActivity.this).
                             setMessage("Para usar essa app é preciso conceder essas permissões").
                             setPositiveButton("OK", new DialogInterface.OnClickListener() {
@@ -116,27 +119,22 @@ public class MainActivity extends AppCompatActivity {
                             }).create().show();
                 }
             }
-        } else {
+        }else {
             MainViewModel vm = new ViewModelProvider(this).get(MainViewModel.class);
             int navigationOpSelected = vm.getNavigationOpSelected();
             bottomNavigationView.setSelectedItemId(navigationOpSelected);
         }
-
     }
 
 
-    private void setFragment(GridViewFragment gridViewFragment) {
-    }
-
-    private void setFragment(ListViewFragment listViewFragment) {
-    }
 
 
-    void setFragment(Fragment Fragment) {
+
+
+    void setFragment(Fragment fragment) {
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.fragContainer, fragment);
         fragmentTransaction.addToBackStack(null);
         fragmentTransaction.commit();
-
     }
 }
